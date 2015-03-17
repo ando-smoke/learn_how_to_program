f3e63d29c4bd67350662a9e578645e50154420cf
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-17 16:09:42 -0700

    Add back in 'show' action to 'sections' routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root :to => 'lessons#index'
 
   resources :lessons do
-    resources :sections, except: [:show, :index]
+    resources :sections, except: [:index] do
+      resources :topics, except: [:show, :index]
+    end
   end
 end
