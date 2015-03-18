d24baea4483cf256d7816c2a532dc7c6a7b06488
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-17 23:23:59 -0700

    Add 'next' method to Topic class def.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,9 +2,15 @@ class Topic < ActiveRecord::Base
   belongs_to :section
   validates :name, presence: true
   validates :topic_text, presence: true
-  validates :sort_id, presence: true
+  validates :sort_id, presence: true, uniqueness: true
 
   def next
-
+    next_topic = self
+    if self.sort_id != Topic.maximum('sort_id')
+      index = self.sort_id + 1
+      next_topic = Topic.where('sort_id > ?', next_topic.sort_id)
+                        .order('sort_id ASC').first
+    end
+    next_topic
   end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,14 +4,40 @@ describe Topic do
   it { should validate_presence_of :name }
   it { should validate_presence_of :topic_text }
   it { should validate_presence_of :sort_id }
+  it { should validate_uniqueness_of :sort_id }
   it { should belong_to :section }
 
-  # context '#next' do
-  #   it 'returns the topic with the next-highest number than ' \
+  context '#next' do
+    it 'returns the topic with the next-highest sort_id than ' \
+      'the current topic' do
+      current_topic = Topic.create(
+        name: 'topic1',
+        topic_text: 'blah blah blah',
+        sort_id: 1
+      )
+      next_topic = Topic.create(
+        name: 'topic2',
+        topic_text: 'blah blah blah',
+        sort_id: 2
+      )
+      expect(current_topic.next).to eq next_topic
+    end
+  end
+
+  # context '#previous' do
+  #   it 'returns the topic with the next-lowest sort_id than ' \
   #     'the current topic' do
-  #     current_topic = Topic.create( name: 'topic1' )
-  #     next_topic = Topic.create( name: 'topic2' )
-  #     expect(current_topic.next).to eq next_topic
+  #     current_topic = Topic.create(
+  #       name: 'topic1',
+  #       topic_text: 'blah blah blah',
+  #       sort_id: 1
+  #     )
+  #     next_topic = Topic.create(
+  #       name: 'topic2',
+  #       topic_text: 'blah blah blah',
+  #       sort_id: 2
+  #     )
+  #     expect(next_topic.previous).to eq current_topic
   #   end
   # end
 end
