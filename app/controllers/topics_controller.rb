a08ae788fa7e82cc46e1a8c7f6f264471a1daf6e
Andy Uppendahl <a.uppendahl@gmail.com>
2015-03-17 21:43:30 -0700

    Add functionality of adding new topic to a section.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,4 +4,27 @@ class TopicsController < ApplicationController
     @section = @lesson.sections.find(params[:section_id])
     @topic = @section.topics.find(params[:id])
   end
+
+  def new
+    @lesson = Lesson.find(params[:lesson_id])
+    @section = @lesson.sections.find(params[:section_id])
+    @topic = @section.topics.new
+  end
+
+  def create
+    @lesson = Lesson.find(params[:lesson_id])
+    @section = @lesson.sections.find(params[:section_id])
+    @topic = @section.topics.new(topic_params)
+    if @topic.save
+      flash[:notice] = 'Topic successfully added!'
+      redirect_to lesson_section_path(@lesson, @section)
+    else
+      render :new
+    end
+  end
+
+  private
+    def topic_params
+      params.require(:topic).permit(:name, :topic_text)
+    end
 end
